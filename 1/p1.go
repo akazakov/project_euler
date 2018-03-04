ebe906f4f1395b766e00842484e1e278f2fa1ef4
Artem Kazakov <kazakov@gmail.com>
2018-03-04 13:13:49 -0500

    P1 via go

---
+++ 1/p1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func sumz(step, max int) int {
+	sum := 0
+	for i := 0; i < max; i += step {
+		sum += i
+	}
+	return sum
+}
+
+
+func main() {
+	threes := sumz(3, 1000)
+	fives := sumz(5, 1000)
+	fivteens := sumz(15, 1000)
+	fmt.Println(threes + fives - fivteens)
+
+}
